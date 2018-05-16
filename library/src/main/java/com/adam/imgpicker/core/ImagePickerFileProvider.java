368b1d4b10e4da1b71b6e824aab3b3ca75397597
yu <yuli@sm.vvip-u.com>
2018-05-16 10:15:07 +0800

    add file provider

---
+++ library/src/main/java/com/adam/imgpicker/core/ImagePickerFileProvider.java
@@ -0,0 +1,10 @@
+package com.adam.imgpicker.core;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * @author yu
+ * @date 2018/5/15
+ */
+public class ImagePickerFileProvider extends FileProvider {
+}
